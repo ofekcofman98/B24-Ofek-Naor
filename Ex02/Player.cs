8056cd87ab2192c898d7e9fb12c6a896515801e6
ofek <ofekcofman98@gmail.com>
2024-06-11 17:05:41 +0300

    add some consts

--- Ex02/Player.cs
+++ Ex02/Player.cs
@@ -8,6 +8,7 @@ namespace GameLogics
     public class Player
     {
         private const int k_AmountOfLastTurnsRemembered = 4;
+        private const int k_OldestItemIndexInList = 0;
 
         private string m_PlayerName;
         private int m_Score;
@@ -44,7 +45,7 @@ namespace GameLogics
         {
             if (m_ComputerAiMemoryList.Count == k_AmountOfLastTurnsRemembered)
             {
-                m_ComputerAiMemoryList.RemoveAt(0); // Remove the oldest item
+                m_ComputerAiMemoryList.RemoveAt(k_OldestItemIndexInList); // Remove the oldest item
             }
             m_ComputerAiMemoryList.Add(new CardsWithIndex(i_CardId, i_Row, i_Column));
         }
